3c21641d01d5d456349273e87329cf6c328c75e7
Maximilian118 <maxcrosby118@gmail.com>
2025-05-24 14:20:30 +0200

    No need to use "target" variable

--- backend/src/bots/discordBot/disocrdBotListeners.ts
+++ backend/src/bots/discordBot/disocrdBotListeners.ts
@@ -1,7 +1,5 @@
 import { Client, Message } from "discord.js"
 
-const bot_name = "automatarr"
-
 export const messageListeners = (client: Client) => {
   client.on("messageCreate", async (message: Message) => {
     if (message.author.bot || !message.guild) return
@@ -11,13 +9,11 @@ export const messageListeners = (client: Client) => {
 
     if (!message.content.startsWith(prefix)) return
 
-    const [command, ...args] = message.content.slice(prefix.length).trim().split(/\s+/)
-
-    const targeted = args.some((arg) => arg.toLowerCase() === bot_name)
+    const [command, ..._args] = message.content.slice(prefix.length).trim().split(/\s+/)
 
     switch (command.toLowerCase()) {
       case "ping":
-        targeted && (await message.channel.send(casePing(client, message)))
+        await message.channel.send(casePing(client, message))
         break
 
       case "hello":
